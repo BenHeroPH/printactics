c4e3987dd056cc6772e427142a0e34927123947c
BenHeroPH <66245928+BenHeroPH@users.noreply.github.com>
2023-05-18 23:41:52 +0800

    found ways to minimize the code

--- shop.php
+++ shop.php
@@ -8,342 +8,48 @@ include("functions/functions.php");
 include("includes/main.php");
 
 ?>
-  <!-- MAIN -->
+
+
+  <!-- Cover imgs -->
   <main>
-    <!-- HERO -->
-    <div class="nero">
-      <div class="nero__heading">
-        <span class="nero__bold"></span>
-      </div>
-      <p class="nero__text">
-      </p>
+    <div class="#">
     </div>
-  </main>
+    <!-- Main -->
+    <div class="wrapper">
+            <h1>Products<h1>
 
-
-<div id="content" ><!-- content Starts -->
-<div class="container" ><!-- container Starts -->
-
-<div class="col-md-12" ><!--- col-md-12 Starts -->
-
-
-
-</div><!--- col-md-12 Ends -->
-
-<div class="col-md-3"><!-- col-md-3 Starts -->
-
-<?php include("includes/sidebar.php"); ?>
-
-</div><!-- col-md-3 Ends -->
-
-<div class="col-md-9" ><!-- col-md-9 Starts --->
-
-
-<?php getProducts(); ?>
-
-</div><!-- row Ends -->
-
-<center><!-- center Starts -->
-
-<ul class="pagination" ><!-- pagination Starts -->
-
-<?php getPaginator(); ?>
-
-</ul><!-- pagination Ends -->
-
-</center><!-- center Ends -->
+    </div>
 
 
 
-</div><!-- col-md-9 Ends --->
+    <div id="content" class="container"><!-- container Starts -->
+
+    <div class="row"><!-- row Starts -->
+    <?php getPro(); ?>
+    </div><!-- row Ends -->
+
+    </div><!-- container Ends -->
 
 
 
-</div><!--- wait Ends -->
-
-</div><!-- container Ends -->
-</div><!-- content Ends -->
 
 
-
-<?php
-
+<?php 
 include("includes/footer.php");
-
 ?>
-
-<script src="js/jquery.min.js"> </script>
-
-<script src="js/bootstrap.min.js"></script>
-
-<script>
-
-$(document).ready(function(){
-
-/// Hide And Show Code Starts ///
-
-$('.nav-toggle').click(function(){
-
-$(".panel-collapse,.collapse-data").slideToggle(700,function(){
-
-if($(this).css('display')=='none'){
-
-$(".hide-show").html('Show');
-
-}
-else{
-
-$(".hide-show").html('Hide');
-
-}
-
-});
-
-});
-
-/// Hide And Show Code Ends ///
-
-/// Search Filters code Starts ///
-
-$(function(){
-
-$.fn.extend({
-
-filterTable: function(){
-
-return this.each(function(){
-
-$(this).on('keyup', function(){
-
-var $this = $(this),
-
-search = $this.val().toLowerCase(),
-
-target = $this.attr('data-filters'),
-
-handle = $(target),
-
-rows = handle.find('li a');
-
-if(search == '') {
-
-rows.show();
-
-} else {
-
-rows.each(function(){
-
-var $this = $(this);
-
-$this.text().toLowerCase().indexOf(search) === -1 ? $this.hide() : $this.show();
-
-});
-
-}
-
-});
-
-});
-
-}
-
-});
-
-$('[data-action="filter"][id="dev-table-filter"]').filterTable();
-
-});
-
-/// Search Filters code Ends ///
-
-});
-
-
-
-$(document).ready(function(){
-
-  // getProducts Function Code Starts
-
-  function getProducts(){
-
-  // Manufacturers Code Starts
-
-    var sPath = '';
-
-var aInputs = $('li').find('.get_manufacturer');
-
-var aKeys = Array();
-
-var aValues = Array();
-
-iKey = 0;
-
-$.each(aInputs,function(key,oInput){
-
-if(oInput.checked){
-
-aKeys[iKey] =  oInput.value
-
-};
-
-iKey++;
-
-});
-
-if(aKeys.length>0){
-
-var sPath = '';
-
-for(var i = 0; i < aKeys.length; i++){
-
-sPath = sPath + 'man[]=' + aKeys[i]+'&';
-
-}
-
-}
-
-// Manufacturers Code ENDS
-
-// Products Categories Code Starts
-
-var aInputs = Array();
-
-var aInputs = $('li').find('.get_p_cat');
-
-var aKeys = Array();
-
-var aValues = Array();
-
-iKey = 0;
-
-$.each(aInputs,function(key,oInput){
-
-if(oInput.checked){
-
-aKeys[iKey] =  oInput.value
-
-};
-
-iKey++;
-
-});
-
-if(aKeys.length>0){
-
-for(var i = 0; i < aKeys.length; i++){
-
-sPath = sPath + 'p_cat[]=' + aKeys[i]+'&';
-
-}
-
-}
-
-// Products Categories Code ENDS
-
-   // Categories Code Starts
-
-var aInputs = Array();
-
-var aInputs = $('li').find('.get_cat');
-
-var aKeys  = Array();
-
-var aValues = Array();
-
-iKey = 0;
-
-    $.each(aInputs,function(key,oInput){
-
-    if(oInput.checked){
-
-    aKeys[iKey] =  oInput.value
-
-};
-
-    iKey++;
-
-});
-
-if(aKeys.length>0){
-
-    for(var i = 0; i < aKeys.length; i++){
-
-    sPath = sPath + 'cat[]=' + aKeys[i]+'&';
-
-}
-
-}
-
-   // Categories Code ENDS
-
-   // Loader Code Starts
-
-$('#wait').html('<img src="images/load.gif">');
-
-// Loader Code ENDS
-
-// ajax Code Starts
-
-$.ajax({
-
-url:"load.php",
-
-method:"POST",
-
-data: sPath+'sAction=getProducts',
-
-success:function(data){
-
- $('#Products').html('');
-
- $('#Products').html(data);
-
- $("#wait").empty();
-
-}
-
-});
-
-    $.ajax({
-url:"load.php",
-method:"POST",
-data: sPath+'sAction=getPaginator',
-success:function(data){
-$('.pagination').html('');
-$('.pagination').html(data);
-}
-
-    });
-
-// ajax Code Ends
-
-   }
-
-   // getProducts Function Code Ends
-
-$('.get_manufacturer').click(function(){
-
-getProducts();
-
-});
-
-
-  $('.get_p_cat').click(function(){
-
-getProducts();
-
-});
-
-$('.get_cat').click(function(){
-
-getProducts();
-
-});
-
-
- });
-
-</script>
-
+  
+  <!--Start of Tawk.to Script-->
+  <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/631ae3fb54f06e12d893a4ac/1gcggl4f8';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+   })();
+  </script>
+<!--End of Tawk.to Script-->
 </body>
-
 </html>
